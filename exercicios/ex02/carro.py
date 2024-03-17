159a8d470e5c523ca84dcdf28cc8caa7c5826f5b
Arthur Gomes <101721044+arthurgomes1k@users.noreply.github.com>
2024-03-17 09:16:32 -0300

    Programação OO - Contrutor e instanciando objetos

---
+++ exercicios/ex02/carro.py
@@ -0,0 +1,7 @@
+class Carro:
+    def __init__(self, modelo, cor, ano):
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano
+
+meu_carro = Carro(modelo='Fusca', cor='Azul', ano=1970)
